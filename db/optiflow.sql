f6c6cb26e0c6d191f72f921f4629df3830ae1fed
Lawrence <lalloyce@gmail.com>
2024-03-26 00:34:26 +0300

    finalized db schema

--- db/optiflow.sql
+++ db/optiflow.sql
@@ -5,7 +5,7 @@ USE optiflow;
 SET time_zone = '+03:00';
 SET NAMES 'utf8mb4';
 
-CREATE TABLE optiflow.users (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE optiflow.users (
 INSERT INTO users (username, password, email) VALUES ('admin', 'D#FR$GG#D', 'lalloyce@gmail.com');
 
 CREATE TABLE borrowers (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     nationalId INT(8) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE borrowers (
     );
 
 CREATE TABLE loans (
-    loan_id serial PRIMARY KEY,
+    loan_id INT AUTO_INCREMENT PRIMARY KEY,
     loan_amount INT NOT NULL,
     interest_rate INT NOT NULL,
     loan_period INT NOT NULL,
@@ -47,21 +47,17 @@ CREATE TABLE loans (
     loan_repayment_date DATE NOT NULL,
     loan_repayment_amount INT NOT NULL,
     loan_penalty_amount DECIMAL(10,2) DEFAULT 0.00,
-    loan_balance INT NOT NULL
+    loan_balance INT NOT NULL,
+    FOREIGN KEY (borrower_id) REFERENCES borrowers(id)
 );
 
-ALTER TABLE loans
-    ADD FOREIGN KEY (borrower_id) REFERENCES borrowers(id);
-
 CREATE TABLE repayments (
-    repayment_id serial PRIMARY KEY,
+    repayment_id INT AUTO_INCREMENT PRIMARY KEY,
     loan_id INT NOT NULL,
     repayment_amount INT NOT NULL,
     repayment_date DATE NOT NULL,
     repayment_status VARCHAR(255) NOT NULL,
-    borrower_id INT
+    borrower_id INT,
+    FOREIGN KEY (loan_id) REFERENCES loans(loan_id),
+    FOREIGN KEY (borrower_id) REFERENCES borrowers(id)
 );
-
-ALTER TABLE repayments
-    ADD FOREIGN KEY (loan_id) REFERENCES loans(loan_id);
-    ADD FOREIGN KEY (borrower_id) REFERENCES borrowers(id);
