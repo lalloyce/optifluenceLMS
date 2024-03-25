456005fa10550076380f414799c32bdf41b32cd9
Lawrence <lalloyce@gmail.com>
2024-03-26 00:23:48 +0300

    update db schema

--- dashboard.php
+++ dashboard.php
@@ -21,16 +21,16 @@ if (!isset($_SESSION['username'])) {
                 <nav>
                     <ul>
                         <li><a href="dashboard.php">Home</a></li>
-                        <li><a href="Members">Members</a></li>
-                        <li><a href="Loans">Loans</a></li>
-                        <li><a href="Reports">Reports</a></li>
+                        <li><a href="#" onclick="showNewMemberForm()">Create New Member</a></li>
+                        <li><a href="Loans">Create New Loan</a></li>
                         <!-- Add a dropdown menu -->
                         <li>
-                            <a href="#">Dropdown</a>
+                            <a href="#">Reports</a>
                             <div class="dropdown-menu">
-                                <a href="#">Link 1</a>
-                                <a href="#">Link 2</a>
-                                <a href="#">Link 3</a>
+                                <a href="#">Repaid Loans</a>
+                                <a href="#">Overdue Loans</a>
+                                <a href="#">All Loans</a>
+                                <a href="#">All Members</a>
                             </div>
                         </li>
                         <!-- Add more navigation links as needed -->
@@ -42,5 +42,11 @@ if (!isset($_SESSION['username'])) {
                     <h1>Dashboard</h1>
                     <p>Welcome to the dashboard!</p>
                 </div>
+                <div id="newMemberFormContainer" style="display: none;">
+                    function showNewMemberForm() {
+                        document.getElementById('newMemberFormContainer').style.display = 'block';
+                    }
+                </div>
+
             </body>
         </html>
--- db/optiflow.sql
+++ db/optiflow.sql
@@ -19,3 +19,49 @@ CREATE TABLE optiflow.users (
 
 INSERT INTO users (username, password, email) VALUES ('admin', 'D#FR$GG#D', 'lalloyce@gmail.com');
 
+CREATE TABLE borrowers (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    nationalId INT(8) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    occupation VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    county VARCHAR(255) NOT NULL,
+    mobileNumber VARCHAR(10) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL
+    );
+
+CREATE TABLE loans (
+    loan_id serial PRIMARY KEY,
+    loan_amount INT NOT NULL,
+    interest_rate INT NOT NULL,
+    loan_period INT NOT NULL,
+    borrower_id INT,
+    loan_type VARCHAR(255) NOT NULL,
+    loan_purpose VARCHAR(255) NOT NULL,
+    loan_status VARCHAR(255) NOT NULL,
+    loan_date DATE NOT NULL,
+    loan_due_date DATE NOT NULL,
+    loan_repayment_date DATE NOT NULL,
+    loan_repayment_amount INT NOT NULL,
+    loan_penalty_amount DECIMAL(10,2) DEFAULT 0.00,
+    loan_balance INT NOT NULL
+);
+
+ALTER TABLE loans
+    ADD FOREIGN KEY (borrower_id) REFERENCES borrowers(id);
+
+CREATE TABLE repayments (
+    repayment_id serial PRIMARY KEY,
+    loan_id INT NOT NULL,
+    repayment_amount INT NOT NULL,
+    repayment_date DATE NOT NULL,
+    repayment_status VARCHAR(255) NOT NULL,
+    borrower_id INT
+);
+
+ALTER TABLE repayments
+    ADD FOREIGN KEY (loan_id) REFERENCES loans(loan_id);
+    ADD FOREIGN KEY (borrower_id) REFERENCES borrowers(id);
