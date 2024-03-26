2e72f12fdb6b95bdc9a97c01084c59e71809fa0e
Lawrence <lalloyce@gmail.com>
2024-03-26 19:20:13 +0300

    simplified loan creation workflow.

--- db/optiflow.sql
+++ db/optiflow.sql
@@ -23,8 +23,7 @@ CREATE TABLE borrowers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    nationalId INT(8) NOT NULL,
-    location VARCHAR(255) NOT NULL,
+    nationalId INT(8) NOT NULL UNIQUE,
     occupation VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
     county VARCHAR(255) NOT NULL,
@@ -35,35 +34,30 @@ CREATE TABLE borrowers (
     );
 
 CREATE TABLE loans (
-    loan_id INT AUTO_INCREMENT PRIMARY KEY,
+    loan_num INT AUTO_INCREMENT PRIMARY KEY,
     loan_amount INT NOT NULL,
     interest_rate INT NOT NULL,
     loan_period INT NOT NULL,
     borrower_id INT,
     loan_type VARCHAR(255) NOT NULL,
-    loan_purpose VARCHAR(255) NOT NULL,
-    loan_status VARCHAR(255) NOT NULL,
+    loan_status VARCHAR(255) NOT NULL DEFAULT 'Active',
     loan_date DATE NOT NULL,
-    loan_due_date DATE NOT NULL,
-    loan_repayment_date DATE NOT NULL,
-    loan_repayment_amount INT NOT NULL,
-    loan_penalty_amount DECIMAL(10,2) DEFAULT 0.00,
+    due_date DATE NOT NULL,
+    closure_date DATE DEFAULT NULL,
+    penalty_amount DECIMAL(10,2) DEFAULT 0.00,
     loan_balance INT NOT NULL,
     FOREIGN KEY (borrower_id) REFERENCES borrowers(nationalId)
 );
 
-INSERT INTO loans (loan_amount, interest_rate, loan_period, borrower_id, loan_type, loan_purpose, loan_status, loan_date, loan_due_date, loan_repayment_date, loan_repayment_amount, loan_penalty_amount, loan_balance)
-VALUES (1000, 5, 12, 1, 'Personal', 'Test', 'Active', CURDATE() - INTERVAL 31 DAY, CURDATE() + INTERVAL 1 YEAR, NULL, 0, 0, 1000);
-
 CREATE TABLE repayments (
-    repayment_id INT AUTO_INCREMENT PRIMARY KEY,
-    loan_id INT NOT NULL,
+    txn_id INT AUTO_INCREMENT PRIMARY KEY,
+    loan_num INT NOT NULL,
     repayment_amount INT NOT NULL,
     repayment_date DATE NOT NULL,
     repayment_status VARCHAR(255) NOT NULL,
     borrower_id INT,
-    FOREIGN KEY (loan_id) REFERENCES loans(loan_id),
-    FOREIGN KEY (borrower_id) REFERENCES borrowers(id)
+    FOREIGN KEY (loan_num) REFERENCES loans(loan_num),
+    FOREIGN KEY (borrower_id) REFERENCES borrowers(nationalId)
 );
 
 DELIMITER //
@@ -73,17 +67,13 @@ DELIMITER //
     DO
       BEGIN
         UPDATE loans
-        SET loan_penalty_amount = loan_balance * 0.1,
-            loan_balance = loan_balance + (loan_balance * 0.1)
+        SET penalty_amount = loan_balance * 0.1,
+            loan_balance = loan_balance + penalty_amount,
+            loan_status = 'Default'
         WHERE DATEDIFF(CURRENT_DATE, loan_date) > 30
         AND loan_balance > 0;
       END //
 DELIMITER ;
-
-alter table borrowers
-    add column age int default 0,
-    add column created_at timestamp default current_timestamp,
-    ADD UNIQUE (nationalId);
 
 DELIMITER //
 CREATE TRIGGER calculate_age_before_insert
